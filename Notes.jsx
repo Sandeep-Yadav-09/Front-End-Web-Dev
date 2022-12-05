2726712e19a72de4fd3f736445f94883720064c6
Sandeep Yadav <111993507+Sandeep-Yadav-09@users.noreply.github.com>
2022-12-05 17:02:59 +0530

    Add files via upload

---
+++ Notes.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+function Notes() {
+  return (
+    <div className = "note">
+      <h1>Deep Learning</h1>
+      <p>
+        Deep Learning is a subfield of machine learning concerned with
+        algorithms inspired by the structure and function of the brain called
+        artificial neural networks.
+      </p>
+    </div>
+  );
+}
+
+export default Notes;
